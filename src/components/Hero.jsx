ec85e971808e94dafd84f8d29eae14d28aac86b1
Ryan Moody <ryanmoody@protonmail.com>
2023-03-20 10:37:39 -0500

    Fix image styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,20 +5,23 @@ import Container from "./Container";
 const Hero = () => {
   return (
     <Container>
-      <section className="flex flex-col-reverse md:flex-row gap-20 items-center py-20">
+      <section className="flex flex-col md:flex-row gap-20 items-center py-20">
         <StaticImage
-          src="../images/profile.jpeg"
           alt="Profile"
-          width={300}
           height={300}
-          className="rounded-full max-h-full"
+          imgClassName="rounded-full max-h-full"
+          layout="fixed"
+          loading="eager"
+          placeholder="none"
+          src="../images/profile.jpeg"
+          width={300}
         />
-        <header className="flex flex-col justify-center items-center md:items-start gap-6">
-          <span className="text-2xl">I'm a</span>
-          <h1 className="font-bold text-7xl md:text-8xl text-center md:text-left">
-            UI Engineer
+        <header className="flex flex-col justify-center md:items-start gap-6">
+          <span className="text-xl">I'm a</span>
+          <h1 className="font-bold text-5xl md:text-6xl md:text-left">
+            Frontend Engineer
           </h1>
-          <span className="text-2xl">
+          <span className="text-xl">
             using code to solve problems for humans.
           </span>
         </header>
